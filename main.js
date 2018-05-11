d4d2086e00c5499063856a64db7840db5c6ae267
ana_marquez <anamarquez.edu@hotmail.com>
2018-05-10 17:06:06 -0700

    init
    Video
    https://www.vuemastery.com/courses/intro-to-vue-js/conditional-rendering

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+        el: "#app",
+        data:{
+        	product:'Socks',
+        	image:'backgroundcolorfull.jpg',
+        	anchortag:'https://www.google.com',
+        	inStock:false
+         }
+  });
