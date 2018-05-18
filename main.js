3f7744998057b218744f9743b2e2e3d5088e62e3
ana_marquez <anamarquez.edu@hotmail.com>
2018-05-17 17:07:53 -0700

    List Rendering         https://www.vuemastery.com/courses/intro-to-vue-js/list-rendering

--- main.js
+++ main.js
@@ -4,6 +4,40 @@ var app = new Vue({
         	product:'Socks',
         	image:'backgroundcolorfull.jpg',
         	anchortag:'https://www.google.com',
-        	inStock:false
-         }
+        	inStock:true,
+        	onSale:false,
+        	detail:["80% cotton","20% polyester","Gender-neutral"],
+        	variants:[
+        	{
+        		variantId:2234,
+        		variantColor: "pink"
+        	},
+        	{
+        		variantId:2235,
+        		variantColor: "black"
+        	}
+        	],
+        	ArraySizes [
+        	        	comentarios:
+        	        	       	{
+        	        	       		comentarioId:1,
+        	        	       		comentario: 'Bueno'
+        	        	       	},
+        	        	comentarios:
+        	        	        {
+        	        	           	comentarioId:2,
+        	        	           	comentario: 'Malo'
+        	        	        },
+        	        	comentarios:
+        	        	       	{
+        	        	       	    comentarioId:3,
+        	        	       	    comentario: 'Okay'
+        	        	       	},
+        	        	 comentarios:  
+        	        	 		{
+        	        	 		    comentarioId:4,
+        	        	 		    comentario: 'Mala calidad'
+        	        	 		},
+        	        	]
+        	}
   });
